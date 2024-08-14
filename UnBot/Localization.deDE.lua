0d55145153cc004c4f00c0ac7e7858ba335c11d3
Dennis <icemansparks@gmail.com>
2024-08-14 13:01:48 +0200

    add new localization files

---
+++ UnBot/Localization.deDE.lua
@@ -0,0 +1,4 @@
+local _, L = ...;
+if GetLocale() == "deDE" then
+    L["Bar is now hidden. Use /unbot to toggle the bar."] = "Die Leiste ist jetzt ausgeblendet. Verwende /unbot, um die Leiste einzublenden.";
+end
---
+++ UnBot/Localization.enUS.lua
@@ -0,0 +1,28 @@
+local _, L = ...;
+if GetLocale() == "enUS" then
+
+    -- UnBot.lua
+    L["Bar is now hidden. Use /unbot to toggle the bar."]   = "Bar is now hidden. Use /unbot to toggle the bar.";
+    L["Window initialization failed: "]                     = "Window initialization failed: ";
+    L["InspectFrame_Show "]                                 = "InspectFrame_Show ";
+    L["Initializing bot controller"]                        = "Initializing bot controller";
+    L["Bot controller initialized"]                         = "Bot controller initialized";
+    L["Target:"]                                            = "Target:";
+    L["Uses:"]                                              = "Uses:";
+    L["LMB: Use"]                                           = "LMB: Use";
+    L["RMB: Toggle button group"]                           = "RMB: Toggle button group";
+    L["RMB: Set shortcut"]                                  = "RMB: Set shortcut";
+    L["RMB: Hide the bar"]                                  = "RMB: Hide the bar";
+
+    L["Select reward"]                                      = "Select reward";
+    L["Could not find order "]                              = "Could not find order ";
+    L["You have no target."]                                = "You have no target.";
+    L["The target is not in your group."]                   = "The target is not in your group.";
+    
+    L["You click button index "]                            = "You click button index ";
+    L[", execute "]                                         = ", execute ";
+    
+
+
+
+end
