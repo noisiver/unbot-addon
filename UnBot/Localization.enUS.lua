67e0a68a7657904e93dcd05f6a16f38f62315ef6
Dennis <icemansparks@gmail.com>
2024-08-14 13:31:18 +0200

    add default translation for UnBotBags.lua

--- UnBot/Localization.enUS.lua
+++ UnBot/Localization.enUS.lua
@@ -21,8 +21,69 @@ if GetLocale() == "enUS" then
     
     L["You click button index "]                            = "You click button index ";
     L[", execute "]                                         = ", execute ";
+
     
+    -----------------------------------------------------------------------------------------------
+    --- UnBotBags.lua ---
+    -----------------------------------------------------------------------------------------------
+    
+    -- UnBotBags.lua -> UnBotGetCostEnergyText
+    L["Consume"] = "Consume";
+    L["Mana"] = "Mana";
+    L["Rage"] = "Rage";
+    L["Energy"] = "Energy";
+    L["Resource"] = "Resource";
 
+    -- UnBotBags.lua -> UnBotShowButtonTips
+    L["item:"] = "item:";
+    L["Quantity owned："] = "Quantity owned：";
 
+    L[" yards"] = " yards";
+    L["Instant"] = "Instant";
+    L["Cast time: "] = "Cast time: ";
+    L[" second(s)"] = " second(s)";
+
+    L["ITEM_NOT_IN_BAG_REFRESH"] = "This item has not appeared in your backpack. You need to query the server. Please click the refresh button at the bottom of the interface to see it.";
+    
+    L["LMB: Equip"] = "LMB: Equip";
+    L["LMB: Discard"] = "LMB: Discard";
+    L["LMB: Sell"] = "LMB: Sell";
+    L["LMB: Use"] = "LMB: Use";
+    L["LMB: Cast"] = "LMB: Cast";
+    L["LMB: Let"] = "LMB: Let";
+
+    L["RMB: Hide"] = "RMB: Hide";
+
+    L["Index:"] = "Index:";
+
+    -- CreateBagsTypeOptions
+    L["View"] = "View";
+    L["Inventory"] = "Inventory";
+    L["Equip"] = "Equip";
+    L["Equip item"] = "Equip item";
+    L["Discard"] = "Discard";
+    L["Discard item"] = "Discard item";
+    L["Sell"] = "Sell";
+    L["Sell item"] = "Sell item";
+    L["Use"] = "Use";
+    L["Use item"] = "Use item";
+
+    -- CreateOptionByParent
+    L["Refresh"] = "Refresh";
+
+    -- UpdateUnBotBagsFramePage
+    L["Page "] = "Page ";
+    L[" of "] = " of ";
+
+    -- ExecuteCommandByBagsItem
+    L["You have no vendor targeted."] = "You have no vendor targeted.";
+
+    -- FlushItemsToBags
+    L["c"] = "c";
+    L["spells"] = "spells";
+
+    -- RecvMuchSpellToBags
+    L["Recv spell id "] = "Recv spell id ";
+    L[" error."] = " error.";
 
 end
