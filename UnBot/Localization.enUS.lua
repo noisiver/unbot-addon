f521fcf9f31b580b5d3a73550d15e13f66ce082f
Dennis <icemansparks@gmail.com>
2024-08-15 10:07:59 +0200

    add translations for Strategy.lua

--- UnBot/Localization.enUS.lua
+++ UnBot/Localization.enUS.lua
@@ -1,26 +1,6 @@
 local _, L = ...;
+
 if GetLocale() == "enUS" then
-
-    -- UnBot.lua
-    L["Bar is now hidden. Use /unbot to toggle the bar."]   = "Bar is now hidden. Use /unbot to toggle the bar.";
-    L["Window initialization failed: "]                     = "Window initialization failed: ";
-    L["InspectFrame_Show "]                                 = "InspectFrame_Show ";
-    L["Initializing bot controller"]                        = "Initializing bot controller";
-    L["Bot controller initialized"]                         = "Bot controller initialized";
-    L["Target:"]                                            = "Target:";
-    L["Uses:"]                                              = "Uses:";
-    L["LMB: Use"]                                           = "LMB: Use";
-    L["RMB: Toggle button group"]                           = "RMB: Toggle button group";
-    L["RMB: Set shortcut"]                                  = "RMB: Set shortcut";
-    L["RMB: Hide the bar"]                                  = "RMB: Hide the bar";
-
-    L["Select reward"]                                      = "Select reward";
-    L["Could not find order "]                              = "Could not find order ";
-    L["You have no target."]                                = "You have no target.";
-    L["The target is not in your group."]                   = "The target is not in your group.";
-    
-    L["You click button index "]                            = "You click button index ";
-    L[", execute "]                                         = ", execute ";
 
     -----------------------------------------------------------------------------------------------
     --- Bindings.lua ---
@@ -111,6 +91,53 @@ if GetLocale() == "enUS" then
     L["Left mouse button: Select "] = "Left mouse button: Select ";
     L[" as the quest reward"] = " as the quest reward";
 
+    -----------------------------------------------------------------------------------------------
+    --- Strategy.lua ---
+    -----------------------------------------------------------------------------------------------
+    
+    -- StrategyOption_OnEnter
+    L["Active"] = "Active";
+    L["Inactive"] = "Inactive";
+    L["Some strategies are ineffective for certain classes and some are mutually exclusive."] = "Some strategies are ineffective for certain classes and some are mutually exclusive.";
+    L["LMB: Deactivate"] = "LMB: Deactivate";
+    L["LMB: Activate"] = "LMB: Activate";
+
+    -----------------------------------------------------------------------------------------------
+    --- StrategyTips.lua ---
+    -----------------------------------------------------------------------------------------------
+    
+    
+
+    -----------------------------------------------------------------------------------------------
+    --- Tooltips.lua ---
+    -----------------------------------------------------------------------------------------------
+    
+    
+
+
+    -----------------------------------------------------------------------------------------------
+    --- UnBot.lua ---
+    -----------------------------------------------------------------------------------------------
+    
+    L["Bar is now hidden. Use /unbot to toggle the bar."]   = "Bar is now hidden. Use /unbot to toggle the bar.";
+    L["Window initialization failed: "]                     = "Window initialization failed: ";
+    L["InspectFrame_Show "]                                 = "InspectFrame_Show ";
+    L["Initializing bot controller"]                        = "Initializing bot controller";
+    L["Bot controller initialized"]                         = "Bot controller initialized";
+    L["Target:"]                                            = "Target:";
+    L["Uses:"]                                              = "Uses:";
+    L["LMB: Use"]                                           = "LMB: Use";
+    L["RMB: Toggle button group"]                           = "RMB: Toggle button group";
+    L["RMB: Set shortcut"]                                  = "RMB: Set shortcut";
+    L["RMB: Hide the bar"]                                  = "RMB: Hide the bar";
+ 
+    L["Select reward"]                                      = "Select reward";
+    L["Could not find order "]                              = "Could not find order ";
+    L["You have no target."]                                = "You have no target.";
+    L["The target is not in your group."]                   = "The target is not in your group.";
+     
+    L["You click button index "]                            = "You click button index ";
+    L[", execute "]                                         = ", execute ";
 
     -----------------------------------------------------------------------------------------------
     --- UnBotBags.lua ---
